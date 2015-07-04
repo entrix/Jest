a8a309ed66ec6a9eae21ba8995e1145a6fce9776
avvolkov <entrix.91@gmail.com>
2015-07-04 22:56:26 +0300

    Add example.

--- test.lisp
+++ test.lisp
@@ -1,1 +1,7 @@
-a + b + c * (v + w)
+(defmethod initialize-instance :after ((account bank-account) &key)
+  (let ((balance (slot-value account 'balance)))
+    (setf (slot-value account 'account-type)
+          (cond
+            ((>= balance 100000) :gold)
+            ((>= balance 50000) :silver)
+            (t :bronze)))))
